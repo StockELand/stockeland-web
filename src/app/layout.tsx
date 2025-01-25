d9dfc6c4470479e6441bb297bce6b6f70aae49fe
l11040 <l1104033837@gmail.com>
2025-01-25 18:48:58 +0900

    [Feature] Header 목업 제작

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from "next";
 import StyledComponentsRegistry from "../lib/registry";
+import { Noto_Sans_KR } from "next/font/google";
+import "./globals.css";
+
+const notoSansKr = Noto_Sans_KR({
+  variable: "--noto_sans_kr",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "StockELand Dashboard",
@@ -12,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>
+      <body className={`${notoSansKr.variable}`}>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,67 @@
+"use client";
+
+import Link from "next/link";
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100vw;
+  height: 64px;
+  padding: 0px 24px;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  width: fit-content;
+  height: 100%;
+  padding: 0px 12px;
+  align-items: center;
+`;
+const LogoText = styled.span`
+  color: #a6fc35;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+const NavContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: fit-content;
+  height: 100%;
+  padding: 0px 24px;
+`;
+const NavItemContainer = styled.div`
+  display: flex;
+  width: fit-content;
+  height: 100%;
+  padding: 0px 12px;
+  align-items: center;
+`;
+const NavText = styled.span`
+  color: white;
+  font-size: 14px;
+  font-weight: bold;
+`;
+
 export default function Home() {
-  return <div>테스트</div>;
+  return (
+    <>
+      <HeaderContainer>
+        <LogoContainer>
+          <Link href={"/"}>
+            <LogoText>StockELand</LogoText>
+          </Link>
+        </LogoContainer>
+        <NavContainer>
+          <NavItemContainer>
+            <NavText>Dashboard</NavText>
+          </NavItemContainer>
+          <NavItemContainer>
+            <NavText>설정</NavText>
+          </NavItemContainer>
+        </NavContainer>
+      </HeaderContainer>
+    </>
+  );
 }
