d11c98efb21f6b66fb87e8ba01cd6d81195c22e3
l11040 <l1104033837@gmail.com>
2025-01-26 21:09:48 +0900

    [Feature] 프리뷰 섹션 목업 구성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import styled from "styled-components";
 
 const HeaderContainer = styled.div`
@@ -44,6 +45,113 @@ const NavText = styled.span`
   font-weight: bold;
 `;
 
+const Heading = styled.h1`
+  font-size: 32px;
+  font-weight: bold;
+  color: white;
+  width: fit-content;
+  height: 80px;
+  display: flex;
+  align-items: center;
+`;
+
+const MainContainer = styled.div`
+  margin: 0px 32px;
+`;
+
+const PreviewContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  column-gap: 24px;
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-radius: 20px;
+  border: solid 1px #2b3139;
+  padding: 24px;
+  min-width: 240px;
+  width: 400px;
+  row-gap: 16px;
+
+  height: fit-content;
+`;
+
+const CardTitleContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: fit-content;
+`;
+const CardTitleText = styled.span`
+  color: white;
+  font-size: 14px;
+  font-weight: bold;
+`;
+const PredictListContainer = styled.div`
+  width: 100%;
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  row-gap: 12px;
+`;
+
+const PredictListItemContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 4px;
+`;
+
+const StockTitleConatiner = styled.div`
+  display: flex;
+  flex-direction: row;
+  column-gap: 12px;
+  align-items: center;
+`;
+
+const StockNameText = styled.span`
+  color: white;
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+const StockImageContainer = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 100%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+`;
+
+const StockRateText = styled.span<{ $isGrowth?: boolean }>`
+  color: ${({ $isGrowth }) => ($isGrowth ? "#a6fc35" : "#F6465D")};
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+const ManualRunContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const ButtonContainer = styled.div`
+  background-color: #fcd535;
+  color: #1e2329;
+  border-radius: 10px;
+  height: 48px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: bold;
+`;
+
 export default function Home() {
   return (
     <>
@@ -62,6 +170,174 @@ export default function Home() {
           </NavItemContainer>
         </NavContainer>
       </HeaderContainer>
+      <MainContainer>
+        <Heading>Dashboard</Heading>
+        <PreviewContainer>
+          <CardContainer>
+            <CardTitleContainer>
+              <CardTitleText>수동 실행</CardTitleText>
+            </CardTitleContainer>
+            <ManualRunContainer>
+              <ButtonContainer>Data Parsing</ButtonContainer>
+              <ButtonContainer>Model Learning</ButtonContainer>
+            </ManualRunContainer>
+          </CardContainer>
+          <CardContainer>
+            <CardTitleContainer>
+              <CardTitleText>Top 5</CardTitleText>
+            </CardTitleContainer>
+            <PredictListContainer>
+              <PredictListItemContainer>
+                <StockTitleConatiner>
+                  <StockImageContainer>
+                    <Image
+                      src="/tesla-logo.png"
+                      alt={""}
+                      width={24}
+                      height={24}
+                    />
+                  </StockImageContainer>
+                  <StockNameText>TSLA</StockNameText>
+                </StockTitleConatiner>
+                <StockRateText $isGrowth>23.19%</StockRateText>
+              </PredictListItemContainer>
+              <PredictListItemContainer>
+                <StockTitleConatiner>
+                  <StockImageContainer>
+                    <Image
+                      src="/tesla-logo.png"
+                      alt={""}
+                      width={24}
+                      height={24}
+                    />
+                  </StockImageContainer>
+                  <StockNameText>TSLA</StockNameText>
+                </StockTitleConatiner>
+                <StockRateText $isGrowth>23.19%</StockRateText>
+              </PredictListItemContainer>
+              <PredictListItemContainer>
+                <StockTitleConatiner>
+                  <StockImageContainer>
+                    <Image
+                      src="/tesla-logo.png"
+                      alt={""}
+                      width={24}
+                      height={24}
+                    />
+                  </StockImageContainer>
+                  <StockNameText>TSLA</StockNameText>
+                </StockTitleConatiner>
+                <StockRateText $isGrowth>23.19%</StockRateText>
+              </PredictListItemContainer>
+              <PredictListItemContainer>
+                <StockTitleConatiner>
+                  <StockImageContainer>
+                    <Image
+                      src="/tesla-logo.png"
+                      alt={""}
+                      width={24}
+                      height={24}
+                    />
+                  </StockImageContainer>
+                  <StockNameText>TSLA</StockNameText>
+                </StockTitleConatiner>
+                <StockRateText $isGrowth>23.19%</StockRateText>
+              </PredictListItemContainer>
+              <PredictListItemContainer>
+                <StockTitleConatiner>
+                  <StockImageContainer>
+                    <Image
+                      src="/tesla-logo.png"
+                      alt={""}
+                      width={24}
+                      height={24}
+                    />
+                  </StockImageContainer>
+                  <StockNameText>TSLA</StockNameText>
+                </StockTitleConatiner>
+                <StockRateText $isGrowth>23.19%</StockRateText>
+              </PredictListItemContainer>
+            </PredictListContainer>
+          </CardContainer>
+          <CardContainer>
+            <CardTitleContainer>
+              <CardTitleText>Bottom 5</CardTitleText>
+            </CardTitleContainer>
+            <PredictListContainer>
+              <PredictListItemContainer>
+                <StockTitleConatiner>
+                  <StockImageContainer>
+                    <Image
+                      src="/tesla-logo.png"
+                      alt={""}
+                      width={24}
+                      height={24}
+                    />
+                  </StockImageContainer>
+                  <StockNameText>TSLA</StockNameText>
+                </StockTitleConatiner>
+                <StockRateText>23.19%</StockRateText>
+              </PredictListItemContainer>
+              <PredictListItemContainer>
+                <StockTitleConatiner>
+                  <StockImageContainer>
+                    <Image
+                      src="/tesla-logo.png"
+                      alt={""}
+                      width={24}
+                      height={24}
+                    />
+                  </StockImageContainer>
+                  <StockNameText>TSLA</StockNameText>
+                </StockTitleConatiner>
+                <StockRateText>23.19%</StockRateText>
+              </PredictListItemContainer>
+              <PredictListItemContainer>
+                <StockTitleConatiner>
+                  <StockImageContainer>
+                    <Image
+                      src="/tesla-logo.png"
+                      alt={""}
+                      width={24}
+                      height={24}
+                    />
+                  </StockImageContainer>
+                  <StockNameText>TSLA</StockNameText>
+                </StockTitleConatiner>
+                <StockRateText>23.19%</StockRateText>
+              </PredictListItemContainer>
+              <PredictListItemContainer>
+                <StockTitleConatiner>
+                  <StockImageContainer>
+                    <Image
+                      src="/tesla-logo.png"
+                      alt={""}
+                      width={24}
+                      height={24}
+                    />
+                  </StockImageContainer>
+                  <StockNameText>TSLA</StockNameText>
+                </StockTitleConatiner>
+                <StockRateText>23.19%</StockRateText>
+              </PredictListItemContainer>
+              <PredictListItemContainer>
+                <StockTitleConatiner>
+                  <StockImageContainer>
+                    <Image
+                      src="/tesla-logo.png"
+                      alt={""}
+                      width={24}
+                      height={24}
+                    />
+                  </StockImageContainer>
+                  <StockNameText>TSLA</StockNameText>
+                </StockTitleConatiner>
+                <StockRateText>23.19%</StockRateText>
+              </PredictListItemContainer>
+            </PredictListContainer>
+          </CardContainer>
+        </PreviewContainer>
+      </MainContainer>
     </>
   );
 }
