64d0725db4ee13ad12a6bff08f64d99576576399
l11040 <l1104033837@gmail.com>
2025-02-03 19:17:41 +0900

    [Update] 임시 파싱 SSE 대응 코드 작성

--- src/components/domain/ManualRunCard.tsx
+++ src/components/domain/ManualRunCard.tsx
@@ -1,21 +1,28 @@
 import Card from "@/components/ui/Card";
 import Button from "@/components/ui/Button";
 import useSSE from "@/hooks/useSSE";
+import { useState } from "react";
 
 export default function ManualRunCard() {
   const { progress, status, startSSE } = useSSE(
-    "http://localhost:8080/sse/progress"
+    "http://localhost:8080/parse/progress"
   );
-  const {
-    progress: modelProgress,
-    status: modelStatus,
-    startSSE: modelStartSSE,
-  } = useSSE("http://localhost:8080/sse/progress");
+  const [isParsing, setIsParsing] = useState(false);
+
+  const startParsing = async () => {
+    setIsParsing(true);
+    try {
+      await fetch("http://localhost:8080/parse/update", { method: "POST" });
+      startSSE();
+    } catch (error) {
+      console.error("Error starting parsing:", error);
+    }
+  };
 
   return (
     <Card title="수동실행">
       <div className="flex flex-col space-y-2">
-        <Button onClick={startSSE} className="overflow-x-auto relative">
+        <Button onClick={startParsing} className="overflow-x-auto relative">
           {status !== "completed" && (
             <div
               className="absolute inset-0 bg-black/20 transition-transform duration-100 ease-linear z-0"
@@ -24,26 +31,14 @@ export default function ManualRunCard() {
           )}
           <div className="relative z-10">
             {status === "completed"
-              ? "Parsing completed"
-              : progress === 0
-              ? "Data Parsing"
-              : `Data Parsing ${progress}%`}
+              ? "Parsing Completed"
+              : isParsing
+              ? `Parsing... ${progress}%`
+              : "Start Parsing"}
           </div>
         </Button>
-        <Button onClick={modelStartSSE} className="overflow-x-auto relative">
-          {modelStatus !== "completed" && (
-            <div
-              className="absolute inset-0 bg-black/20 transition-transform duration-100 ease-linear z-0"
-              style={{ transform: `translateX(${-100 + modelProgress}%)` }}
-            />
-          )}
-          <div className="relative z-10">
-            {modelStatus === "completed"
-              ? "Learning Completed"
-              : modelProgress === 0
-              ? "Model Learning"
-              : `Model Learning ${modelProgress}%`}
-          </div>
+        <Button className="overflow-x-auto relative">
+          <div className="relative z-10">Model Learning</div>
         </Button>
       </div>
     </Card>
