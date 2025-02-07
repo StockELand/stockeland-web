99d58be8d2d7959fd2522977447acd68ab983f58
l11040 <113540683+l11040@users.noreply.github.com>
2025-02-07 12:55:03 +0900

    Feature/apply sse to parsing (#5)
    * [Update] 임시 파싱 SSE 대응 코드 작성
    
    * [Update] 임시 SSE 구성

--- src/components/domain/ManualRunCard.tsx
+++ src/components/domain/ManualRunCard.tsx
@@ -1,48 +1,73 @@
 import Card from "@/components/ui/Card";
 import Button from "@/components/ui/Button";
 import useSSE from "@/hooks/useSSE";
+import { useState } from "react";
 
 export default function ManualRunCard() {
   const { progress, status, startSSE } = useSSE(
-    "http://localhost:8080/sse/progress"
+    "http://localhost:8080/parse/progress"
   );
+
   const {
-    progress: modelProgress,
-    status: modelStatus,
-    startSSE: modelStartSSE,
-  } = useSSE("http://localhost:8080/sse/progress");
+    progress: predictProgress,
+    status: predictStatus,
+    startSSE: predictStartSSE,
+  } = useSSE("http://localhost:8080/predict/progress");
+
+  const [isParsing, setIsParsing] = useState(false);
+  const [isLearning, setIsLearning] = useState(false);
+
+  const startParsing = async () => {
+    setIsParsing(true);
+    try {
+      startSSE();
+      await fetch("http://localhost:8080/parse/update", { method: "POST" });
+    } catch (error) {
+      console.error("Error starting parsing:", error);
+    }
+  };
+
+  const startLearning = async () => {
+    setIsLearning(true);
+    try {
+      predictStartSSE();
+      await fetch("http://localhost:8080/predict/update", { method: "POST" });
+    } catch (error) {
+      console.error("Error starting Learning:", error);
+    }
+  };
 
   return (
     <Card title="수동실행">
       <div className="flex flex-col space-y-2">
-        <Button onClick={startSSE} className="overflow-x-auto relative">
+        <Button onClick={startParsing} className="overflow-x-auto relative">
           {status !== "completed" && (
             <div
-              className="absolute inset-0 bg-black/20 transition-transform duration-100 ease-linear z-0"
-              style={{ transform: `translateX(${-100 + progress}%)` }}
+              className="absolute inset-0 bg-rise text-xs h-full text-inverseForground text-center leading-none"
+              style={{ width: `${progress}%` }}
             />
           )}
           <div className="relative z-10">
             {status === "completed"
-              ? "Parsing completed"
-              : progress === 0
-              ? "Data Parsing"
-              : `Data Parsing ${progress}%`}
+              ? "Parsing Completed"
+              : isParsing
+              ? `Parsing ${status}... ${progress}%`
+              : "Start Parsing"}
           </div>
         </Button>
-        <Button onClick={modelStartSSE} className="overflow-x-auto relative">
-          {modelStatus !== "completed" && (
+        <Button onClick={startLearning} className="overflow-x-auto relative">
+          {predictStatus !== "completed" && (
             <div
-              className="absolute inset-0 bg-black/20 transition-transform duration-100 ease-linear z-0"
-              style={{ transform: `translateX(${-100 + modelProgress}%)` }}
+              className="absolute inset-0 bg-rise text-xs h-full text-inverseForground text-center leading-none"
+              style={{ width: `${predictProgress}%` }}
             />
           )}
           <div className="relative z-10">
-            {modelStatus === "completed"
+            {predictStatus === "completed"
               ? "Learning Completed"
-              : modelProgress === 0
-              ? "Model Learning"
-              : `Model Learning ${modelProgress}%`}
+              : isLearning
+              ? `Learning ${predictStatus}... ${predictProgress}%`
+              : "Start Learning"}
           </div>
         </Button>
       </div>
