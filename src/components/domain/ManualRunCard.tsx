9239154b673eb2ee83b9eb450928c231116f5f4a
l11040 <l1104033837@gmail.com>
2025-02-07 12:49:55 +0900

    [Update] 임시 SSE 구성

--- src/components/domain/ManualRunCard.tsx
+++ src/components/domain/ManualRunCard.tsx
@@ -7,15 +7,33 @@ export default function ManualRunCard() {
   const { progress, status, startSSE } = useSSE(
     "http://localhost:8080/parse/progress"
   );
+
+  const {
+    progress: predictProgress,
+    status: predictStatus,
+    startSSE: predictStartSSE,
+  } = useSSE("http://localhost:8080/predict/progress");
+
   const [isParsing, setIsParsing] = useState(false);
+  const [isLearning, setIsLearning] = useState(false);
 
   const startParsing = async () => {
     setIsParsing(true);
     try {
-      await fetch("http://localhost:8080/parse/update", { method: "POST" });
       startSSE();
+      await fetch("http://localhost:8080/parse/update", { method: "POST" });
     } catch (error) {
       console.error("Error starting parsing:", error);
+    }
+  };
+
+  const startLearning = async () => {
+    setIsLearning(true);
+    try {
+      predictStartSSE();
+      await fetch("http://localhost:8080/predict/update", { method: "POST" });
+    } catch (error) {
+      console.error("Error starting Learning:", error);
     }
   };
 
@@ -25,20 +43,32 @@ export default function ManualRunCard() {
         <Button onClick={startParsing} className="overflow-x-auto relative">
           {status !== "completed" && (
             <div
-              className="absolute inset-0 bg-black/20 transition-transform duration-100 ease-linear z-0"
-              style={{ transform: `translateX(${-100 + progress}%)` }}
+              className="absolute inset-0 bg-rise text-xs h-full text-inverseForground text-center leading-none"
+              style={{ width: `${progress}%` }}
             />
           )}
           <div className="relative z-10">
             {status === "completed"
               ? "Parsing Completed"
               : isParsing
-              ? `Parsing... ${progress}%`
+              ? `Parsing ${status}... ${progress}%`
               : "Start Parsing"}
           </div>
         </Button>
-        <Button className="overflow-x-auto relative">
-          <div className="relative z-10">Model Learning</div>
+        <Button onClick={startLearning} className="overflow-x-auto relative">
+          {predictStatus !== "completed" && (
+            <div
+              className="absolute inset-0 bg-rise text-xs h-full text-inverseForground text-center leading-none"
+              style={{ width: `${predictProgress}%` }}
+            />
+          )}
+          <div className="relative z-10">
+            {predictStatus === "completed"
+              ? "Learning Completed"
+              : isLearning
+              ? `Learning ${predictStatus}... ${predictProgress}%`
+              : "Start Learning"}
+          </div>
         </Button>
       </div>
     </Card>
