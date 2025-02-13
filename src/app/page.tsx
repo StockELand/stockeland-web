2ef8fd67c2155bd678b40fc39a8cca6cb7afec0d
l11040 <l1104033837@gmail.com>
2025-02-13 19:41:40 +0900

    [Add] Process 페이지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,6 @@ import ManualRunCard from "@/components/domain/ManualRunCard";
 import StockListCard from "@/components/domain/StockListCard";
 import useSWR from "swr";
 import { IStockPrediction } from "@/types/api";
-import { DatePicker } from "@/components/date-picker";
-
 export default function Home() {
   const { data: predictions } = useSWR<IStockPrediction[]>(
     "http://localhost:8080/stock/predictions"
@@ -16,22 +14,6 @@ export default function Home() {
 
   return (
     <>
-      <DatePicker
-        doubleCalendar
-        displayDateGroups={[
-          { type: "today", dates: [new Date()], color: "#fcd535" },
-          {
-            type: "success",
-            dates: [new Date(2025, 1, 15), new Date(2025, 1, 20)],
-            color: "#a6fc35",
-          },
-          {
-            type: "failure",
-            dates: [new Date(2025, 1, 18), new Date(2025, 1, 22)],
-            color: "#F6465D",
-          },
-        ]}
-      />
       <Typography variant="h2">Dashboard</Typography>
       <div className="flex flex-wrap gap-6 mb-6">
         <ManualRunCard />
