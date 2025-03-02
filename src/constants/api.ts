99dd22c74041950552c72802304b693fa5018521
l11040 <l1104033837@gmail.com>
2025-03-02 14:34:57 +0900

    [Chore]

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8080";
+// export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "http://192.168.0.21:8080";
 
 export const API = {
   PREDICT: {
